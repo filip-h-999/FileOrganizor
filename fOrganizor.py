ab5e54fc14cd850d830fa25ac4aaf361438b3f7a
filip-h-999 <filip.hrenovcik@gmail.com>
2023-04-30 19:55:29 +0200

    fixing tuple bug and adding more options

--- fOrganizor.py
+++ fOrganizor.py
@@ -9,17 +9,25 @@ import sys
 def organize(path):
     correctPath = False
 
-    audio = (".mp3", ".wav", ".aac", ".flac", ".wma")
-    video = (".mp4", ".avi", ".mov", ".wmv", ".mkv")
-    image = (".jpg", ".png", ".gif", ".bmp", ".svg")
-    documents = (".doc", ".docx", ".pdf")
-    pwp = (".ppt", ".pptx")
+    audio = [".mp3", ".wav", ".aac", ".flac", ".wma"]
+    video = [".mp4", ".avi", ".mov", ".wmv", ".mkv"]
+    image = [".jpg", ".png", ".gif", ".bmp", ".svg"]
+    documents = [".doc", ".docx", ".pdf", "odt"]
+    zipF =[".zip,", ".7zip"]
+    pwp = [".ppt", ".pptx"]
+    text = [".txt"]
+    icon = [".ico"]
+    exe = [".exe"]
             
     is_audio = lambda file: os.path.splitext(file)[1] in audio
     is_video = lambda file: os.path.splitext(file)[1] in video
     is_image = lambda file: os.path.splitext(file)[1] in image
     is_documents = lambda file: os.path.splitext(file)[1] in documents
     is_pwp = lambda file: os.path.splitext(file)[1] in pwp
+    is_txt = lambda file: os.path.splitext(file)[1] in text
+    is_zip = lambda file: os.path.splitext(file)[1] in zipF
+    is_exe = lambda file: os.path.splitext(file)[1] in exe
+    is_icon = lambda file: os.path.splitext(file)[1] in icon
 
     # def is_audio(file):
     #     return os.path.splitext(file) [1] in audio # returns True if exrtention is in audio
@@ -37,6 +45,8 @@ def organize(path):
         correctPath = False
 
     for file in os.listdir():
+        if file.isdir(path): 
+            continue
         if is_audio(file):
             moveFile("audio")
         elif is_video(file):
@@ -47,6 +57,14 @@ def organize(path):
             moveFile("documents")
         elif is_pwp(file):
             moveFile("pwp")
+        elif is_txt(file):
+            moveFile("text")
+        elif is_zip(file):
+            moveFile("zipF")
+        elif is_icon(file):
+            moveFile("icon")
+        elif is_exe(file):
+            moveFile("exe")
     
     return correctPath
     
